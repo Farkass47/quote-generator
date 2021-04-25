706afe62ef068d5e29b12969c0de5fe6cea386b4
Fasa47 <sandor.farkas.fasa47@gmail.com>
2021-04-25 11:35:47 +0200

    1st refactor

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-let apiQuotes = [];
+let quotes = [];
+const QUOTE_SOURCE = "LOCAL";
 
-// Show loading
-function loading(){
+function showLoadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide loading
-function complete(){
+function removeLoadingSpinner(){
     loader.hidden = true;
     quoteContainer.hidden = false;
+}
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
 }
 
 function insertQuote(quote){
@@ -31,43 +35,31 @@ function insertQuote(quote){
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 }
 
-//Show newQuote
 function newQuote() {
-    loading();
-    // Pick a random Quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    showLoadingSpinner();
+    const quote = quotes[Math.floor(Math.random()*quotes.length)];
     insertQuote(quote);
 }
 
-// Get Quotes from API
 async function getQuotes() {
-    loading();
-    const apiUrl = 'https://type.fit/api/quotes';
+    showLoadingSpinner();
+
     try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
+        if (QUOTE_SOURCE === 'LOCAL') {
+            quotes = (await import('./quotes.js')).localQuotes
+        } else {
+            const apiUrl = 'https://type.fit/api/quotes';
+            const response = await fetch(apiUrl);
+            quotes = await response.json();
+        }
         newQuote();
     } catch(error) {
-        //Catch Error Here
         console.log(error.message);
-        complete();
+        removeLoadingSpinner();
     }
-}
-
-function newLocalQuotes() {
-    loading();
-    // Pick a random Quote from localQuotes array
-    const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
-    insertQuote(quote);
-}
-
-// Tweet Quote
-function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
 }
 
 // Eventlisteners
@@ -75,7 +67,4 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuotes();
-//newLocalQuotes();
-
-
+getQuotes()
